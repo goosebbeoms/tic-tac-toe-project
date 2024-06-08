bf079005a4c7b213da1602cee21c12db5d355373
goosebbeoms <seo980916@gmail.com>
2024-06-08 16:33:25 +0900

    84. Props에서 State 파생하기

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -4,7 +4,16 @@ const initialGameBoard = [
   [null, null, null]
 ]
 
-export default function GameBoard({ onSelectSquare }) {
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard
+
+  for (const turn of turns) {
+    const { square, player } = turn
+    const { row, col } = square
+
+    gameBoard[row][col] = player
+  }
+
   // const [ gameBoard, setGameBoard ] = useState(initialGameBoard)
 
   // function handleSelectSquare(rowIndex, colIndex) {
@@ -21,7 +30,7 @@ export default function GameBoard({ onSelectSquare }) {
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
         <ol>
-          {row.map((playerSymbal, colIndex) => <li key={colIndex}><button onClick={onSelectSquare}>{playerSymbal}</button></li>)}
+          {row.map((playerSymbal, colIndex) => <li key={colIndex}><button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbal}</button></li>)}
         </ol>
       </li>)}
     </ol>
